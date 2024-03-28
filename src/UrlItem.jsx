3d70b1482219a9f3e00c8ae47364ee6a168687b3
“mineii” <kmaoo0406@naver.com>
2024-03-28 09:21:50 +0900

    chore : MSW 세팅

--- src/UrlItem.jsx
+++ src/UrlItem.jsx
@@ -3,13 +3,13 @@ import React from "react";
 function URLitem() {
   return (
     <div>
-      <button>X</button>
       <b>Origin URL</b>
       <a href="연결할 링크의 경로"> 내용 </a>
       <div>
         <b>Short URL</b>
         <a href="연결할 링크의 경로"> 내용 </a>
       </div>
+      <button>X</button>
     </div>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start();
+}
+
+enableMocking().then(() => {
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
